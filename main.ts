71665c25d050610da79163af3f941eabc5dad058
tlauouvea <61261560+tlauouvea@users.noreply.github.com>
2020-07-24 09:46:52 +1100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+let Y = 0
+led.plotBrightness(0, 0, 255)
+for (let X = 0; X <= 4; X++) {
+    for (let YY = 0; YY <= 4; YY++) {
+        Y = 4 - YY
+        if (true) {
+        	
+        }
+    }
+}
+basic.forever(function () {
+	
+})
